b9565c685178c5d1b0c6ca58d24ca72b6d40ec8c
19965254384 <1345424480@qq.com>
2023-02-12 17:38:59 +0800

    “红黑树写完”

---
+++ 红黑树/test.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include "RedBlackTree.h"
+using namespace std;
+int main()
+{
+    sht::RBTree<int, int> t;
+    vector<int> v = {4, 2, 6, 1, 3, 5, 15, 7, 16, 14};
+    t.insert(make_pair(4, 0));
+
+    t.insert(make_pair(2, 0));
+
+    t.insert(make_pair(6, 0));
+    t.insert(make_pair(1, 0));
+    t.BFS();
+}
